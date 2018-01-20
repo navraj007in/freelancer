ebe49ca0b92c525aca64336c015686da07836785
Navraj Singh <navraj24031981@gmail.com>
2018-01-20 23:58:31 +0530

    SB Site Added

---
+++ FreelancerMarketplace/SBSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SBSite.Startup))]
+namespace SBSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
