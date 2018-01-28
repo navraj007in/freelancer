3eee2158e0d2fe21545da99595ff1e577bc54bc7
Navraj Singh <navraj24031981@gmail.com>
2018-01-28 19:14:29 +0530

    Profile Edit Feature

--- FreelancerMarketplace/SugarSite/Controllers/ProfileController.cs
+++ FreelancerMarketplace/SugarSite/Controllers/ProfileController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,21 +10,60 @@ namespace SugarSite.Controllers
 {
     public class ProfileController : Controller
     {
+        Entities db = new Entities();
         // GET: Profile
         public ActionResult Index()
         {
-            return View();
+            string userid = HttpContext.User.Identity.GetUserId();
+            var profile = (from x in db.Profiles
+                           where x.userId == userid
+                           select x).FirstOrDefault();
+
+            return View(profile);
         }
 
         public ActionResult SugarBaby()
         {
-            return View();
+            string userid = HttpContext.User.Identity.GetUserId();
+            var profile = (from x in db.Profiles
+                           where x.userId == userid
+                           select x).FirstOrDefault();
+
+            return View(profile);
         }
 
         public ActionResult SugarDaddy()
         {
-            return View();
-        }
+            string userid = HttpContext.User.Identity.GetUserId();
+            var profile = (from x in db.Profiles
+                           where x.userId == userid
+            select x).FirstOrDefault();
 
+            return View(profile);
+        }
+        [HttpPost]
+        public ActionResult SugarDaddy(Profile profile)
+        {
+            string userid = HttpContext.User.Identity.GetUserId();
+            
+            
+           
+            try
+            {
+                db.Entry(profile).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        System.Console.WriteLine("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                    }
+                }
+            }
+            return View(profile);
+        }
     }
 }
